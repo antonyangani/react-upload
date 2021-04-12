4ef9cbcdca4e8f1c90f4e4ff6224bafd718faffe
anthony-ubuntu <anthony@groctech.com>
2021-04-12 19:29:37 +0300

    Added querying parameters

--- server/db.py
+++ server/db.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, TIMESTAMP, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import text
 
 
 Base = declarative_base()
@@ -11,8 +12,9 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "person"
 
-    id = Column('id', Integer, primary_key=True)
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
     username = Column('username', String, unique=True)
+    last_login_date = Column(TIMESTAMP(timezone=False), server_default=func.now(), nullable=False)
 
 
 engine = create_engine('sqlite:///users.db', echo=True)
@@ -31,5 +33,11 @@ session = Session()
 # session.add(user)
 # session.commit()
 
-users = session.query(person).all()
 
+
+users = session.query(User).all()
+
+for user in users:
+    print(f"The username is {user.username} and id: {user.id} and they last logged in at: {user.last_login_date}")
+
+session.close()
